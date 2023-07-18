8e146358d3335bee65522d3fb91c7d40ef685b23
ThaddeusJW <138039631+ThaddeusJW@users.noreply.github.com>
2023-07-18 12:15:08 -0400

    Able to loop through all pits in rotation JSON

--- rotation.py
+++ rotation.py
@@ -1,8 +1,18 @@
 import json
 
-path = "C:/Users/Thaddeus/Coding/VS Code/Rotation_Checker/showTVData.json"
+# Setting the file path for the JSON file and opening it as a json object
+path = "C:/Users/Thaddeus/Coding/VS Code/Rotation_Checker/showTVData.json" 
 f = open(path, 'r')
 rotation_json = json.load(f)
 
 rotation_str = json.dumps(rotation_json, indent=2)
-print(rotation_str)
+
+
+def table_list():
+    for model in rotation_json['models']: #Loop through all pits
+        for dealer in model['dealerToTableList']: #Loop through all dealers in all pits
+            dealer_name = dealer['dealerName'] #Set dealer name to variable
+            print(dealer_name) 
+
+    
+table_list()
