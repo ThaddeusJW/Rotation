438d3632643b468b7ee97c0fe13876e02a8061f8
ThaddeusJW <138039631+ThaddeusJW@users.noreply.github.com>
2023-07-18 12:24:19 -0400

    Able to loop through all pits in rotation JSON

--- rotation.py
+++ rotation.py
@@ -11,7 +11,7 @@ rotation_str = json.dumps(rotation_json, indent=2)
 def table_list():
     for model in rotation_json['models']: #Loop through all pits
         for dealer in model['dealerToTableList']: #Loop through all dealers in all pits
-            dealer_name = dealer['dealerName'] #Set dealer name to variable
+            dealer_name = dealer['dealerName'] #Set current dealer name to variable
             print(dealer_name) 
 
     
